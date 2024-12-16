e79540b02133b9fe0bdaa70d720882f691e6285b
arshmusic <bchat837@gmail.com>
2024-12-17 00:45:40 +0530

    Update play.py

--- DAXXMUSIC/utils/inline/play.py
+++ DAXXMUSIC/utils/inline/play.py
@@ -66,7 +66,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        ,
+        
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
@@ -82,32 +82,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-                 [
-            InlineKeyboardButton(
-
-                text="✪sᴜᴘᴘᴏʀᴛ✪",
-
-                url=f"https://t.me/PURVI_SUPPORT",
-
-            ),
-            InlineKeyboardButton(
-
-                text="✪ᴄʜᴀᴛ✪",
-
-                url=f"https://t.me/CRAZY_OP_02",
-
-            ),
-        ],
-
-                 [
-            InlineKeyboardButton(
-
-                text="✪ᴏᴡɴᴇʀ✪",
-
-                url=f"t.me/ll_THUNDER_lll",
-
-            ),
-        ],
+        
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
